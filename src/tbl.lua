d1aa5f6dc238b584a03b123abfdf2000f1d710ba
Tim Menzies <timm@ieee.org>
2020-12-06 23:06:26 -0500

    saving

--- src/tbl.lua
+++ src/tbl.lua
@@ -1,6 +1,7 @@
 -- <img width=75 src="https://cdn0.iconfinder.com/data/icons/data-charts/110/TableDataGridThickLines-512.png">   <hr>
 -- <a href="http://github.com/timm/keys"><img src="https://github.blog/wp-content/uploads/2008/12/forkme_left_red_aa0000.png?resize=149%2C149" align=left></a>  
 -- "Keys = cluster, discretize, elites, contrast"    
+-- [![DOI](https://zenodo.org/badge/318809834.svg)](https://zenodo.org/badge/latestdoi/318809834)  
 -- ![](https://img.shields.io/badge/platform-osx%20,%20linux-orange)    
 -- ![](https://img.shields.io/badge/language-lua,bash-blue)  
 -- ![](https://img.shields.io/badge/purpose-ai%20,%20se-blueviolet)  
