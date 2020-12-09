89b4e506b7a21b01c09a060d380c0ef7c07c8b8e
Tim Menzies <timm@ieee.org>
2020-12-09 13:31:27 -0500

    saving

--- src/tbl.lua
+++ src/tbl.lua
@@ -1,7 +1,7 @@
 -- <img width=75 src="https://github.com/timm/keys/raw/main/etc/img/tbl.png"> <hr>
 -- "Keys = cluster, discretize, elites, contrast"    
 -- <a href="http://github.com/timm/keys"><img src="https://github.blog/wp-content/uploads/2008/12/forkme_left_red_aa0000.png?resize=149%2C149" align=left></a>  
--- [![DOI](https://zenodo.org/badge/318809834.svg)](https://zenodo.org/badge/latestdoi/318809834)  sss     
+-- [![DOI](https://zenodo.org/badge/318809834.svg)](https://zenodo.org/badge/latestdoi/318809834)  
 -- ![](https://img.shields.io/badge/platform-osx%20,%20linux-orange)    
 -- ![](https://img.shields.io/badge/language-lua,bash-blue)  
 -- ![](https://img.shields.io/badge/purpose-ai%20,%20se-blueviolet)  
