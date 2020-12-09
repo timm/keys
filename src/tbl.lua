bd15bae3fc1fd7512364e134df51bf45240a1bae
Tim Menzies <timm@ieee.org>
2020-12-09 13:29:21 -0500

    saving

--- src/tbl.lua
+++ src/tbl.lua
@@ -1,7 +1,7 @@
 -- <img width=75 src="https://github.com/timm/keys/raw/main/etc/img/tbl.png"> <hr>
 -- <a href="http://github.com/timm/keys"><img src="https://github.blog/wp-content/uploads/2008/12/forkme_left_red_aa0000.png?resize=149%2C149" align=left></a>  
--- "Keys = cluster, discretize, elites, contrast"    
 
+-- "Keys = cluster, discretize, elites, contrast"    
 -- [![DOI](https://zenodo.org/badge/318809834.svg)](https://zenodo.org/badge/latestdoi/318809834)  sss     
 -- ![](https://img.shields.io/badge/platform-osx%20,%20linux-orange)    
 -- ![](https://img.shields.io/badge/language-lua,bash-blue)  
@@ -10,7 +10,6 @@
 -- ![](https://img.shields.io/badge/license-mit-lightgrey)  
 -- [home](http://menzies.us/keys)         :: [lib](http://menzies.us/keys/lib.html) ::
 -- [cols](http://menzies.us/keys/cols.html) :: [tbl](http://menzies.us/keys/tbl.html)   
-
 --------------------
 local Of  = {
   synopois= "tables with rows, summarized in column headers",
