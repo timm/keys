507ca5449e180fb18e7b7272c63f4069a3b31883
Tim Menzies <timm@ieee.org>
2021-05-01 21:56:32 -0400

    save

--- src/eg.lua
+++ src/eg.lua
@@ -22,6 +22,15 @@ local eg={}
  
 -- <a name=some>
 -- Test if we can approximate a million numbers with a few samples. 
+function eg.lists()
+  math.randomseed(1)
+  assert(lib.has("bb",{"aa","bb","cc"}))
+  print(1)
+  assert(not lib.has("kk",{"aa","bb","cc"}))
+  print(2)
+  print(lib.any {10,20,30}) 
+  assert(30 == lib.any {10,20,30}) 
+end
 
 function eg.some()
   -- load data
@@ -52,11 +61,6 @@ function eg.cliffs()
       if not Stats.cliffsDelta(x,y) then
         printf("for %5s items, things change after delta= %5s", n, d);
         break end end end end
-
-function eg.lists()
-  assert(lib.has("bb",{"aa","bb","cc"}))
-  assert(not lib.has("kk",{"aa","bb","cc"}))
-  assert(30 == lib.any {10,20,30}) end
 
 function eg.split()
   local str = "a,b,c,d"
