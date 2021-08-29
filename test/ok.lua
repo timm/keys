ab4ceec10d2e9e21c96d5cfa3b80f3590c6ead71
Tim Menzies <timm@ieee.org>
2021-08-28 22:49:45 -0400

    save

--- test/ok.lua
+++ test/ok.lua
@@ -10,12 +10,12 @@ local fails=0
 for _,f in pairs(arg) do
    if f ~= "ok.lua" and f ~= "lua" then
      rand.srand(the.seed)
-     local status,err=  pcall(function () dofile(f) end) 
-     if status then
+     local ok,msg=  pcall(function () dofile(f) end) 
+     if ok then
        str.color("green","✔ %s",f)
      else
       fails=fails+1
-      str.color("red","✘ %s",tostring(err)) end end end 
+      str.color("red","✘ %s",tostring(msg)) end end end 
 
 for k,_ in pairs(_ENV) do if not b4[k] then print("?? "..k) end end
 os.exit(fails)
