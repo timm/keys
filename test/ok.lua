401b99a291a8b68525dfa1a3fcf10b37a23cce2d
Tim Menzies <timm@ieee.org>
2021-08-30 09:08:44 -0400

    save

--- test/ok.lua
+++ test/ok.lua
@@ -12,9 +12,9 @@ for _,f in pairs(arg) do
      rand.srand(the.seed)
      local ok,msg=  pcall(function () dofile(f) end) 
      if   ok 
-     then str.color("green","✔ %s",f)
+     then str.color("green","%s",f)
      else fails=fails+1
-          str.color("red","✘ %s",tostring(msg)) end end end 
+          str.color("red","%s",tostring(msg)) end end end 
 
 for k,_ in pairs(_ENV) do if not b4[k] then print("?? "..k) end end
 os.exit(fails)
